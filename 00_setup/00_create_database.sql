a7f06db84c863a9381f3780bae6abea0045a659c
toad <codeoftoad@gmail.com>
2026-05-09 00:52:16 -0400

    Add database setup scripts

--- 00_setup/00_create_database.sql
+++ 00_setup/00_create_database.sql
@@ -1,5 +1,24 @@
+/*
+00_create_database.sql
+
+Purpose:
+Create the RetailOpsPractice database if it does not already exist.
+
+Run this first.
+*/
+
 USE master;
 GO
 
-CREATE DATABASE RetailOpsPractice;
+IF DB_ID(N'RetailOpsPractice') IS NULL
+BEGIN
+	CREATE DATABASE RetailOpsPractice;
+END;
+GO
+
+SELECT
+	name AS DatabaseName,
+	create_date AS CreatedAt
+FROM sys.databases
+WHERE name = N'RetailOpsPractice';
 GO
